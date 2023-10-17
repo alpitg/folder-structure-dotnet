5c90eb0231e361aee273ad3ea0f83aede136bef8
alpitg <alpitgajbhiye@gmail.com>
2023-10-17 22:16:38 +0530

    null tenant

--- src/Structure.Data/Entities/Tenant/Tenant.cs
+++ src/Structure.Data/Entities/Tenant/Tenant.cs
@@ -4,7 +4,7 @@ namespace Structure.Data
 
     public class Tenant : BaseEntity
     {
-        public Guid Id { get; set; }
+        public Guid? Id { get; set; }
         public string? Name { get; set; }
         public string? Email { get; set; }
         public string? ConnectionString { get; set; }
