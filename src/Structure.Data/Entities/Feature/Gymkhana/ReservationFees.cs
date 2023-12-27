6d5ca6864d12e6fc4c09a704e99e18188e3b8b92
alpitg <alpitgajbhiye@gmail.com>
2023-12-27 17:49:33 +0530

    gymkhana base model added

---
+++ src/Structure.Data/Entities/Feature/Gymkhana/ReservationFees.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Structure.Data.Entities.Feature.Gymkhana
+{
+	public class ReservationFees
+	{
+		public Guid? Id { get; set; }
+		public Guid? FacilityId { get; set; }
+        public bool? IsAc { get; set; }
+		public int? Daily { get; set; }
+		public int? Monthly { get; set; }
+		public int? Yearly { get; set; }
+		public string? DailyDisclaimer { get; set; }
+		public string? MonthlyDisclaimer { get; set; }
+		public string? YearlyDisclaimer { get; set; }
+    }
+}
+
---
+++ src/Structure.Data/Entities/Feature/Gymkhana/Timings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Structure.Data.Entities.Feature.Gymkhana
+{
+	public class Timings
+    {
+        public Guid? Id { get; set; }
+        public Guid? FacilityId { get; set; }
+        public byte? Start { get; set; }
+        public byte? End { get; set; }
+        public bool? IsWeeekend { get; set; }
+        public string? Gender { get; set; }
+    }
+}
+
